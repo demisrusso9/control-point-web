37bcad9a5bb91c99318f9de9b180d1be64f19af9
Demis Russo <demis.silva@compasso.com.br>
2021-09-22 16:52:33 -0300

    removing some errors in API folder

--- src/pages/api/database/database.ts
+++ src/pages/api/database/database.ts
@@ -5,10 +5,7 @@ let cachedDb: Db = null
 export default async function connectToDatabase(uri: string) {
   if (cachedDb) return cachedDb
 
-  const client = await MongoClient.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+  const client = await MongoClient.connect(uri)
 
   const dbName = new URL(uri).pathname.substring(1)
   const db = client.db(dbName)
--- src/pages/api/delete.ts
+++ src/pages/api/delete.ts
@@ -6,6 +6,9 @@ export default async (req: VercelRequest, res: VercelResponse) => {
   const db = await connectToDatabase(process.env.MONGODB_URI)
   const collection = db.collection('point')
 
+  // @ts-ignore
+  // I don't know why there is an error in ObjectId,
+  // if I put only req.query.id is not going to delete
   await collection.deleteOne({ _id: ObjectId(req.query.id) })
 
   return res.status(200).json({ message: 'Point Deleted' })
